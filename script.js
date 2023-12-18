fce95851cc5f9e84344a1516e451fb6e19b38991
Jemma  Nzekwe <jemmanzekwe@hotmail.com>
2023-12-18 21:59:28 +0000

    function to colour-code timeblocks

--- script.js
+++ script.js
@@ -2,4 +2,20 @@ $(document).ready(function () {
     // Display current date using Day.js
     $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
+ // Function to color-code timeblocks based on past, present, and future
+ function updateHourBlocks() {
+    var currentHour = dayjs().hour();
+
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("data-hour"));
+
+        if (blockHour < currentHour) {
+            $(this).removeClass("present future").addClass("past");
+        } else if (blockHour === currentHour) {
+            $(this).removeClass("past future").addClass("present");
+        } else {
+            $(this).removeClass("past present").addClass("future");
+        }
+    });
 }
+
