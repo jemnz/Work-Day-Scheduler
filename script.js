4889283deff0d3c38310c03686b4a563ad38595a
Jemma  Nzekwe <jemmanzekwe@hotmail.com>
2023-12-18 22:00:19 +0000

    function to save to local storage

--- script.js
+++ script.js
@@ -2,20 +2,27 @@ $(document).ready(function () {
     // Display current date using Day.js
     $("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
- // Function to color-code timeblocks based on past, present, and future
- function updateHourBlocks() {
-    var currentHour = dayjs().hour();
+// Create timeblocks dynamically (adjust as needed)
+for (var i = 9; i <= 17; i++) {
+    var timeblockDiv = $("<div>").addClass("row time-block").attr("data-hour", i);
 
-    $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("data-hour"));
+    var hourDiv = $("<div>").addClass("col-md-2 hour").text(dayjs().hour(i).format("hA"));
 
-        if (blockHour < currentHour) {
-            $(this).removeClass("present future").addClass("past");
-        } else if (blockHour === currentHour) {
-            $(this).removeClass("past future").addClass("present");
-        } else {
-            $(this).removeClass("past present").addClass("future");
-        }
-    });
+    // Add a unique id to each textarea
+    var textarea = $("<textarea>").addClass("col-md-8").attr("id", "event_" + i);
+
+    var saveBtn = $("<button>").addClass("col-md-2 saveBtn").text("Save");
+
+    timeblockDiv.append(hourDiv, textarea, saveBtn);
+    $(".container").append(timeblockDiv);
 }
 
+
+// Function to save event to local storage
+$(".saveBtn").on("click", function () {
+    var timeblock = $(this).closest(".time-block");
+    var blockHour = timeblock.attr("data-hour");
+    var eventText = timeblock.find("textarea").val();
+
+    localStorage.setItem("event_" + blockHour, eventText);
+});
